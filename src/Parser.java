731271166010379f00c901d5d978e9f34ea09acd
jramorim1 <32297820+jramorim1@users.noreply.github.com>
2018-02-08 12:43:47 -0200

    Add files via upload

--- src/Parser.java
+++ src/Parser.java
@@ -1,4 +1,95 @@
+import java.io.IOException;
 
 public class Parser {
+	
+	private Token currentToken;
+	private Scanner scanner;
+	
+	public Parser(String path) throws IOException{
+		
+		scanner = new Scanner(path);
+		currentToken = scanner.scan();
+	}
+	
+	private void parsePrograma(){
+		
+	}
+	
+	private void parseComando(){
+		
+	}
+	
+	private void parseComandoComposto(){
+		
+	}
+    
+	private void parseCondicional(){
+		
+	}
+	
+    private void parseCorpo(){
+		
+	}
+    
+    private void parseDeclaracao(){
+		
+	}
+    
+    private void parseDeclaracaoDeFuncao(){
+		
+	}
+    
+    private void parseDeclaracaoDeProcedimento(){
+		
+	}
+    
+    private void parseDeclaracaoDeVariavel(){
+		
+	}
+    
+    private void parseExpressao(){
+		
+	}   
+    
+    private void parseFator(){
+		
+	}
+    
+    private void parseIterativo(){
+		
+	}
+    
+    
+    private void parseListaDeExpressoes(){
+		
+	}
+	
+    private void parseListaDeIDs(){
+		
+	}
+    
+    private void parseListaDeParametros(){
+		
+	}
+    
+    private void parseParametros(){
+		
+	}
+    
+    private void parseTermo(){
+		
+	}
+    
+    private void parseTipo(){
+		
+	}
+    
+    private void parseTipoAgregado(){
+		
+	}
+    
+    private void parseTipoSimples(){
+		
+	}
 
 }
